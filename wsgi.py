c969b6cdd3c30fb17f930f6f4eb316970fc9efde
james audretsch <jamesaud@gmail.com>
2019-01-18 18:26:20 +0300

    finally got it to work on heroku

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from app.main import app
 import os
 if __name__ == "__main__":
     PORT = os.environ['PORT']
-    app.run(host='0.0.0.0', debug=True, port=PORT)
+    app.run(host='0.0.0.0', debug=True, port=PORT) 
